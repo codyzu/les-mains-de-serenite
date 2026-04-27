daf756cd2a62423e607b565182129fa6f46685a6
Cody Zuschlag <cody.zuschlag@gmail.com>
2026-04-27 14:56:25 +0200

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import unoCSS from 'unocss/astro';
 import {getSiteBasePath} from './config/base-path.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lesmainsdeserenite.fr',
   base: getSiteBasePath(),
   integrations: [
+    sitemap(),
     unoCSS({
       injectReset: '@unocss/reset/tailwind-v4.css',
     }),
